03b5c90f1140b195ffde1d868359ba6eff8d4a8b
igor <Igor.23457@yandex.ru>
2023-12-19 12:25:09 +0300

    add code

---
+++ server.py
@@ -0,0 +1,23 @@
+import time,socket
+main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+main_socket.bind(("localhost", 10000))
+main_socket.setblocking(False)
+main_socket.listen(5)
+print("Socket")
+players = []
+while True:
+    try:
+        new_socket, addr = main_socket.accept()
+        print("Connected to:", addr)
+        new_socket.setblocking(False)
+        players.append(addr)
+    except BlockingIOError:
+        pass
+    for sock in players:
+        try:
+            data = sock.recv(1024).decode()
+            print("ready:", data)
+        except:
+            pass
+    time.sleep(1)
