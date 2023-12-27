3e03005de9ad96b849c7915099143d6f06041bf2
igor <Igor.23457@yandex.ru>
2023-12-27 10:36:47 +0300

    add code

--- client.py
+++ client.py
@@ -1,7 +1,38 @@
 import socket
+import pygame
+import math
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 sock.connect(("localhost", 10000))
-while True:
-    sock.send("Привет!".encode())
+pygame.init()
+x = 800
+y = 600
+radius = 50
+BLACK = (0, 0, 0)
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("baktery")
+run = True
+old_vector = (0, 0)
+center = (x//2, y//2)
+while run:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            run = False
+            pygame.quit()
+    if pygame.mouse.get_focused():
+        pos = pygame.mouse.get_pos()
+        vector = pos[0] - center[0], pos[1] - center[1]
+        len_ = math.sqrt(vector[0]**2+vector[1]**2)
+        if len_ <= radius:
+            vector = 0, 0
+        if vector != old_vector:
+            old_vector = vector
+            message = "<{},{}>".format(vector[0], vector[1])
+            sock.send(message.encode())
+    screen.fill(BLACK)
+    pygame.draw.circle(screen, (0, 255, 0), center, radius)
+    pygame.display.update()
+    data = sock.recv(1024).decode()
+    print("Получено: ", data)
+pygame.quit()
 
--- server.py
+++ server.py
@@ -1,14 +1,18 @@
-import time,socket
+import time
+
+import socket
+
 main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 main_socket.bind(("localhost", 10000))
 main_socket.setblocking(False)
-main_socket.listen(5)
+main_socket.listen(10)
 print("Socket")
 players = []
 while True:
     try:
-        new_socket, addr = main_socket.accept()
+        new_socket = main_socket.accept()[0]
+        addr = main_socket.accept()[1]
         print("Connected to:", addr)
         new_socket.setblocking(False)
         players.append(addr)
@@ -16,8 +20,9 @@ while True:
         pass
     for sock in players:
         try:
-            data = sock.recv(1024).decode()
-            print("ready:", data)
+            sock.send("ready".encode())
         except:
-            pass
-    time.sleep(1)
+            players.remove(sock)
+            sock.close()
+            print("Closed")
+    #time.sleep(1)
