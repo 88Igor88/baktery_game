edbe8fd30f9eed45338daace0d9c5cbc066e3912
igor <9@tfvfvf.gv>
2024-02-04 20:26:54 +0300

    add code

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import pygame
 import math
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-sock.connect(("localhost", 10000))
+sock.connect(("localhost", 17700))
 pygame.init()
 x = 800
 y = 600
--- server.py
+++ server.py
@@ -4,14 +4,16 @@ from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import declarative_base, sessionmaker
 import psycopg2
 import pygame
+import sys
 
 pygame.init()
 main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-main_socket.bind(("localhost", 10000))
+main_socket.bind(("localhost", 17700))
 main_socket.setblocking(False)
 main_socket.listen(10)
 WIDHT_ROOM, HEIGHT_ROOM = 4000, 4000
+WIDHT_SERVER: int
 WIDHT_SERVER, HEIGHT_SERVER = 300, 300
 screen = pygame.display.set_mode([WIDHT_SERVER, HEIGHT_SERVER])
 
@@ -34,10 +36,12 @@ class Players(Base):
     speed_x = Column(Integer, default=0)
     speed_y = Column(Integer, default=0)
 
-    def __init__(self, adres, name):
+    def __init__(self, adres, id_, name):
+        global ID
         self.name = name
         self.adres = adres
-
+        self.id = ID + 1
+        ID += 1
 
 class LocalPlayer():
     def __init__(self, id, name, socket, addr):
@@ -55,6 +59,10 @@ class LocalPlayer():
         self.speed_y = 0
 
 
+ID = s.query(Players).order_by(Players.id.desc()).limit(1)
+ID = ID.scalar()
+if ID is None:
+    ID = 0
 run = True
 players = {}
 timer = pygame.time.Clock()
@@ -69,7 +77,7 @@ while run:
         new_socket, addr = main_socket.accept()  # принимаем входящие
         print('Подключился', addr)
         new_socket.setblocking(False)
-        new_player = Players(new_socket, addr)
+        new_player = Players(addr, id, "user")  # new_socket, addr)
         s.merge(new_player)
         s.commit()
         addr = f'({addr[0]},{addr[1]})'
@@ -82,9 +90,9 @@ while run:
         print(e)
 
     # Считываем команды игроков
-    for id in list(players):
+    for id_ in list(players):
         try:
-            data = players[id].sock.recv(1024).decode()
+            data = players[id_].sock.recv(1024).decode()
             print("Получил", data)
         except:
             pass
@@ -98,7 +106,19 @@ while run:
             s.query(Players).filter(Players.id == id).delete()
             s.commit()
             print("Сокет закрыт")
-    timer.tick(60)
+
+    screen.fill((0, 0, 0))
+    for id_ in players:
+        player = players[id_]
+        x = player.x * WIDHT_SERVER // WIDHT_ROOM
+        y = player.y * HEIGHT_SERVER // HEIGHT_ROOM
+        size = player.size * WIDHT_SERVER // WIDHT_ROOM
+        pygame.draw.circle(screen, (23, 23, 0), (x, y), size)
     pygame.display.update()
-    time.sleep(1)
+    pygame.display.flip()
+    timer.tick(100)
 pygame.quit()
+main_socket.close()
+s.query(Players).delete()
+s.commit()
+sys.exit()
